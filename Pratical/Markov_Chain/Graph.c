60516bf6c35355889dce4e112593599c32a1976d
Andre Mendes <andre.f.mendes24@gmail.com>
2021-09-14 02:13:56 +0100

    Version 0.9 of Graph implementation

---
+++ Pratical/Markov_Chain/Graph.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "Graph.h"
+#include "Bag.h"
+
+Graph *graph_create(int num_vertices)
+{
+    Graph *graph = malloc(sizeof(Graph));
+
+    if(graph != NULL)
+    {
+        graph->num_vertices = num_vertices;
+        graph->num_edges = 0;
+        graph->adj = malloc(num_vertices * sizeof(Bag));
+
+        for(int v = 0; v < num_vertices; v++)
+        {
+            graph->adj[v] = bag_create();
+        }
+    }
+
+    return graph;
+}
+
+Graph *graph_fill(FILE *input)
+{
+    Graph *graph;
+
+    char temp_buff[8];
+    if(fgets(temp_buff, sizeof(temp_buff), input) == NULL)  // Reading number of vertices
+    {                                                       // from file
+        return NULL;
+    }
+
+    int num_vertices = atoi(temp_buff);
+    graph = graph_create(num_vertices);
+
+    fgets(temp_buff, sizeof(temp_buff), input);             // Reading number of edges
+    int num_edges = atoi(temp_buff);                        // from file
+
+    for(int i = 0; i < num_edges; i++)
+    {
+        fgets(temp_buff, sizeof(temp_buff), input);
+
+        char v_char = temp_buff[0];
+        char w_char = temp_buff[2];
+
+        if(isdigit(v_char) && isdigit(w_char))
+        {
+            int v = v_char - '0';
+            int w = w_char - '0';
+            
+            add_edge(graph, v, w);
+        }
+    }
+
+    return graph;
+}
+
+int vertices(Graph *graph)
+{
+    return graph->num_vertices;
+}
+
+int edges(Graph *graph)
+{
+    return graph->num_edges;
+}
+
+void add_edge(Graph *graph, int v, int w)
+{
+    if(v >= graph->num_vertices || w >= graph->num_vertices)
+    {
+        printf("ERROR: Vertex Pair: (%d, %d) must be within vertex limit of %d\n", v, w, graph->num_vertices);
+        exit(1);
+    }
+
+    int *v_2 = (int *) malloc(1 * sizeof(w));
+    int *w_2 = (int *) malloc(1 * sizeof(v));
+
+    *v_2 = v;
+    *w_2 = w;
+
+    bag_add(graph->adj[v], w_2);
+    bag_add(graph->adj[w], v_2);
+
+    graph->num_edges++;
+}
+
+Bag *adj(Graph *graph, int v)
+{
+    return graph->adj[v];
+}
+
+char **graph_print(Graph *graph)
+{
+    return NULL;
+}
---
+++ Pratical/Markov_Chain/Graph.h
@@ -0,0 +1,52 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "Bag.h"
+
+typedef struct graph_t
+{
+    int num_vertices;
+    int num_edges;
+    Bag **adj;
+} Graph;
+
+/**
+ * Create a graph
+ * 
+ * @returns Pointer to a new, empty graph. If allocation fails returns NULL
+*/ 
+Graph *graph_create(int num_vertices);
+
+/**
+ * Create a graph and add the edges from a FILE
+ * 
+ * @returns Pointer to new, constructed graph. If allocation fails returns NULL
+*/
+Graph *graph_fill(FILE *input);
+
+/**
+ *  @returns Number of vertices
+*/
+int vertices(Graph *graph);
+
+/**
+ * @returns Number of edges
+*/
+int edges(Graph *graph);
+
+/**
+ * Adds an edge between two vertices
+*/
+void add_edge(Graph *graph, int v1, int v2);
+
+/**
+ * @returns The Adjanceny List of the Graph at position v
+*/
+Bag *adj(Graph *graph, int v);
+
+/**
+ * Prints a representation of the graph as an Adjacency List
+*/
+char **graph_print(Graph *graph);
+
+#endif
